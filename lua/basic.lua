da11bf2125bc38208abfc7e6cc183c4a60946245
zzjzzj1 <2361553935@qq.com>
2023-05-09 19:14:51 +0800

    fix icons

--- lua/basic.lua
+++ lua/basic.lua
@@ -9,11 +9,7 @@ vim.wo.number = true
 vim.wo.relativenumber = false
 -- 高亮所在行
 vim.wo.cursorline = true
--- 显示左侧图标指示列
-vim.wo.signcolumn = "yes"
--- 右侧参考线，超过表示代码太长了，考虑换行
--- vim.wo.colorcolumn = "80"
--- 缩进2个空格等于一个Tab
+-- 缩进4个空格等于一个Tab
 vim.o.tabstop = 4
 vim.bo.tabstop = 4
 vim.o.softtabstop = 4
